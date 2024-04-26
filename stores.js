5482454e866531d098636ccdafe927a0ba5a7e12
ashivam0011 <161700017+ashivam0011@users.noreply.github.com>
2024-04-26 06:54:11 +0530

    no changes

---
+++ stores.js
@@ -0,0 +1,38 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.5904237024862, 23.1621213525281]
+        },
+        "properties": {
+          "name": "Venue 1",
+          "address": "Adalaj Stepwell, Adalaj, Gandhinagar, Gujarat 382421, India The Adalaj Stepwell, also known as Rudabai Stepwell, is a stunning five-story stepwell located in Adalaj village near Gandhinagar. Built in the 15th century by Queen Rudabai, it is renowned for its intricate carvings, architectural brilliance, and the harmonious blend of Hindu and Islamic styles. This stepwell served as a place for socializing, spiritual reflection, and water conservation, making it a significant historical and cultural landmark in Gujarat.",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.82815459698692, 18.94324557965778]
+        },
+        "properties": {
+          "name": "Venue 2",
+          "address": "Shop No 7, Ground Floor, Chemox House, Hospital Lane, Barrack Rd, New Marine Lines, Mumbai, Maharashtra 400020, India",
+          "phone": "23 2323 2323"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [72.81416419471911, 18.958588300624903]
+        },
+        "properties": {
+          "name": "Venue 3",
+          "address": "Mathew Rd, Charni Road East, Opera House, Gamdevi, Mumbai, Maharashtra 400004, India",
+          "phone": "23 2323 2323"
+        }
+      }
+]
